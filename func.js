9ec584cfb1dda190209b5ed4a2a935cc653be6e3
sasha <ashanina@u.rochester.edu>
2016-01-08 19:05:28 -0500

    changed to top , thanks steve

--- func.js
+++ func.js
@@ -5,44 +5,39 @@ function clean() {
 	$('#aboutme').hide();
 	$('#art').hide();
 }
+
+ // {
+	// 		progress: function(now,fx) {
+	// 		  $(this).css('transform','translateY(0%)'); 
+	// 		}
+	// 	}, 
+
 $(document).ready(clean);
 function show(id_clicked, id1, id2) {
 	// clean();
 	if (menu_is_up == false) {
 		$('hr.top').hide();
 		$('hr.bottom').hide();
-		$('.'+id_clicked+'_circle').animate({
-			marginTop: "1%",
-			marginRight: "18px", 
-			height: "70px",
-			width: "70px",
-			borderRadius: "35px"	
-		}, {
-	    	duration: 240
-		});
+		$('.'+id_clicked+'_circle').addClass("AnimateSlow");
+		// $('.'+id_clicked+'_circle').animate({
+		// 	top: "0%",
+		// 	// transform: translateY("0%"),
+		// 	// webkitTransform: translateY(0%),
+		// 	marginRight: "28px", 
+		// 	height: "70px",
+		// 	width: "70px",
+		// 	borderRadius: "35px"	
+		// }, 340, "swing");
+		// // $('.'+id_clicked+'_circle').css({transform: "translateY(0%)"});
 		$('#'+id_clicked+'_icon').removeClass("fa-3x").addClass("fa-2x");
-		$('.'+id1+'_circle').animate({
-			marginTop: "1%",
-			marginRight: "18px",
-			height: "70px",
-			width: "70px",
-			borderRadius: "35px"
-		}, {
-	    	duration: 130
-		});
-		$('#'+id1+'_icon').removeClass("fa-3x").addClass("fa-2x");
-		$('.'+id2+'_circle').animate({
-			marginTop: "1%",
-			marginRight: "18px",
-			height: "70px",
-			width: "70px",
-			borderRadius: "35px"
-		}, {
-	    	duration: 130
-		});
-		$('#'+id2+'_icon').removeClass("fa-3x").addClass("fa-2x");
 
-		$('hr.bottom').fadeIn(1000);
+		$('.'+id1+'_circle').addClass("AnimateFast");
+		$('#'+id1+'_icon').removeClass("fa-3x").addClass("fa-2x");
+
+		$('.'+id2+'_circle').addClass("AnimateFast");
+		$('#'+id2+'_icon').removeClass("fa-3x").addClass("fa-2x");
+		$('hr.bottom').css({top: "11%"});
+		$('hr.bottom').delay(500).fadeIn(2000);
 		menu_is_up = true;
 	}
 
@@ -97,8 +92,6 @@ function change_background_projects(id) {
 	});
 
 	$('.'+id+'_circle').css({'background-image': 'url("imgs/main_'+id+'.jpg")', 'background-size': size});
-
-
 
 }
 
